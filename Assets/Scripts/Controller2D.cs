f1557ffcc6da776660484b00723bab2781f8579c
Kirill Marchuk <62mkv@mail.ru>
2021-01-05 21:35:05 +0200

    EP-04: Climbing Slopes (DONE)

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Controller2D : MonoBehaviour
 {
     const float skinWidth = .015f;
+    const float maxClimbAngle = 60;
 
     public int horizontalRayCount = 4;
     public int verticalRayCount = 4;
@@ -62,11 +63,32 @@ public class Controller2D : MonoBehaviour
 
             if (hit)
             {
-                velocity.x = (hit.distance - skinWidth) * directionX;
-                rayLength = hit.distance;
+                float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+                
+                if (i == 0 && slopeAngle <= maxClimbAngle)
+                {
+                    float distanceToSlopeStart = 0;
+                    if (slopeAngle != collisions.slopeAngleOld)
+                    {
+                        distanceToSlopeStart = hit.distance - skinWidth;
+                        velocity.x -= distanceToSlopeStart * directionX;
+                    }
+                    ClimbSlope(ref velocity, slopeAngle);
+                    velocity.x += distanceToSlopeStart * directionX;
+                }
 
-                collisions.left = directionX < 0;
-                collisions.right = directionX > 0;
+                if (!collisions.climbingSlope || slopeAngle > maxClimbAngle)
+                {
+                    velocity.x = (hit.distance - skinWidth) * directionX;
+                    rayLength = hit.distance;
+
+                    if (collisions.climbingSlope)
+                    {
+                        velocity.y = Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x);
+                    }
+                    collisions.left = directionX < 0;
+                    collisions.right = directionX > 0;
+                }
             }
         }
     }
@@ -88,9 +110,29 @@ public class Controller2D : MonoBehaviour
                 velocity.y = (hit.distance - skinWidth) * directionY;
                 rayLength = hit.distance;
 
+                if (collisions.climbingSlope)
+                {
+                    velocity.x = velocity.y / Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Sign(velocity.x);
+                }
                 collisions.below = directionY < 0;
                 collisions.above = directionY > 0;
             }
+        }
+    }
+
+    void ClimbSlope(ref Vector3 velocity, float slopeAngle)
+    {
+        float moveDistance = Mathf.Abs(velocity.x);
+        float climbVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+        if (velocity.y <= climbVelocityY)
+        {
+            velocity.y = climbVelocityY;
+            velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * velocity.x;
+
+            // as long as we're "climbing a slope" we can assume we're already on the ground, so...
+            collisions.below = true;
+            collisions.climbingSlope = true;
+            collisions.slopeAngle = slopeAngle;
         }
     }
 
@@ -123,11 +165,18 @@ public class Controller2D : MonoBehaviour
     {
         public bool left, right;
         public bool above, below;
+        public bool climbingSlope;
+
+        public float slopeAngle, slopeAngleOld;
 
         public void Reset()
         {
             left = right = false;
             above = below = false;
+            climbingSlope = false;
+
+            slopeAngleOld = slopeAngle;
+            slopeAngle = 0;
         }
     }
 
